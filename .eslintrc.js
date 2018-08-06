64b639aedb024c1d6e686d7dc821364bd247a103
anon <sarah.anne.friend@gmail.com>
2018-08-06 16:47:45 -0400

    Basic deps, eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    "max-len": ["error", 130],
+    "comma-dangle": "off",
+    "arrow-body-style": ["off"]
+  }
+};
