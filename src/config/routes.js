63e3202ced0c666fcd203fbdc194cb195b73b909
anon <sarah.anne.friend@gmail.com>
2018-08-18 15:54:26 -0400

    Lay in cognito express, basic auth middleware

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
-import { defaultAppMsg } from './env'
+import { defaultAppMsg } from './env';
+import authMiddleware from '../middleware/auth';
 
 export default function (app) {
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -7,4 +8,7 @@ export default function (app) {
 
   app.get('/', (req, res) => res.status(200).json(defaultAppMsg));
 
+  // define a separate router for this, if most calls end up being authenticated
+  app.get('/test', authMiddleware, (req, res) => res.status(200).json('Auth was successful!'));
+  
 }
