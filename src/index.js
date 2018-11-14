ac84eb4f184e330759865ae446793c31abedcd8a
anon <sarah.anne.friend@gmail.com>
2018-11-14 15:03:47 -0500

    Connection working with orm

--- src/index.js
+++ src/index.js
@@ -2,27 +2,21 @@ import express from 'express';
 import routes from './config/routes';
 import log from './lib/logger';
 import { port } from './config/env';
+import knex from './connections/postgres';
 
-const { Pool } = require('pg');
-// pools will use environment variables
-// for connection information
-const pool = new Pool();
-
-async function getDBTime () {
-  console.log('pool query');
-  const res = await pool.query('SELECT NOW()');
-  await pool.end();
-  console.log('pool query res', res);
-  return res;
+const getDBTime = async () => {
+  const res = await knex.raw('SELECT NOW()')
+  return res.rows[0].now;
 }
 
 const app = express();
-
 routes(app);
 
-app.listen(port, (err) => {
+app.listen(port, async (err) => {
   if (err) log.error(err);
   else log.info(`Ed Julio & Sarah are listening on :${port}`);
 
-  log.info('getDBTime', getDBTime());
+  // database check, can be removed soon
+  const dbTime = await getDBTime();
+  log.info(`Database time is ${dbTime}`);
 });
