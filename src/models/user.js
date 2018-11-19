9587a9f2b24a63e30cdf1b31f58527df7cdd519f
anon <sarah.anne.friend@gmail.com>
2018-11-19 11:34:08 -0500

    Scaffold migrations

---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+import CirclesModel from '../lib/postgresModels';
+
+export default class Asset extends BalanceModel {
+  static get tableName() { return 'user'; }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      properties: {
+        id: { type: 'integer' },
+        agreedToDisclaimer: { type: 'boolean' }, //used for legal reasons, and to denote that the user has been fully set up
+        createdAt: { type: 'object' },
+        updatedAt: { type: 'object' }
+        displayName: { type: 'string' },
+        email: { type: 'string' },
+        profilePicURL: { type: 'string' },
+      },
+    };
+  }
+}
