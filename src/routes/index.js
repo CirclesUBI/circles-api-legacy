307dc241f31e1ff47e9c61a108f0ae160e95fd27
ed murphy <edzillion@gmail.com>
2018-12-13 22:52:44 +0100

    cors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 import bodyParser from 'body-parser';
 import authMiddleware from '../middleware/auth';
 import hasPermissionMiddleware from '../middleware/permissions';
-
+import cors from 'cors';
 import userRouter from './userRouter'
 
 const versionString = '/v1'
 
 export default function (app) {
+  app.use(cors());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+  app.use(bodyParser.json());  
 
-  app.get('/', (req, res) => res.status(200).json(defaultAppMsg));
+  app.get('/', (req, res) => res.status(200).json('hello Ed!'));
 
   // define a separate router for this, if most calls end up being authenticated
   app.get('/test',
