be028ff2c7011404203459562c911b2597b5f826
ed murphy <edzillion@gmail.com>
2018-12-14 01:37:33 +0100

    auth route for user calls

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,9 @@ import bodyParser from 'body-parser';
 import authMiddleware from '../middleware/auth';
 // import hasPermissionMiddleware from '../middleware/permissions';
 import cors from 'cors';
-// import userRouter from './userRouter'
+import userRouter from './userRouter'
 
-// const versionString = '/v1'
+const versionString = '/v1'
 
 export default function (app) {
   app.use(cors());
@@ -12,12 +12,7 @@ export default function (app) {
   app.use(bodyParser.json());  
 
   app.get('/', (req, res) => res.status(200).json('hello Ed!'));
-
-  // define a separate router for this, if most calls end up being authenticated
-  app.get('/test',
-    authMiddleware,
-    // hasPermissionMiddleware('ownUser', 'read'),
-    (req, res) => res.status(200).json('Auth was successful!'));
-
-  // app.use(versionString + '/user', userRouter)
+  
+  app.use(authMiddleware)
+  app.use(versionString + '/user', userRouter)
 }
