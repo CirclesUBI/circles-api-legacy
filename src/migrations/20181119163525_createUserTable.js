4ec025932ec1ccb098955d3e5e2f829256046904
ed murphy <edzillion@gmail.com>
2018-12-17 14:38:36 +0100

    organization api, migrations and seeds

--- src/migrations/20181119163525_createUserTable.js
+++ src/migrations/20181119163525_createUserTable.js
@@ -1,19 +1,40 @@
 
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('user', (t) => {
-    t.string('id').primary();
-    t.string('display_name', 100).notNullable();
-    t.string('email', 50).notNullable();
-    t.string('profile_pic_url', 500).notNullable();
-    t.dateTime('created_at').notNullable();
-    t.dateTime('updated_at');
-    t.boolean('agreed_to_disclaimer').notNullable().defaultTo(false);
-    t.string('device_id').notNullable();
-    t.string('device_endpoint');
-    t.string('phone_number').notNullable();
-  });
-};
+    t.string('id').primary()
+    t.string('display_name', 100).notNullable()
+    t.string('email', 50).notNullable()
+    t.string('profile_pic_url', 500).notNullable()
+    t.dateTime('created_at').notNullable().defaultTo(knex.fn.now())
+    t.dateTime('updated_at')
+    t.boolean('agreed_to_disclaimer').notNullable().defaultTo(false)
+    t.string('device_id').notNullable()
+    t.string('device_endpoint')
+    t.string('phone_number').notNullable()
+  }).createTable('organization', (t) => {
+    t.string('id').primary()
+    t.string('organization_name', 100).notNullable()
+    t.string('email', 50).notNullable()
+    t.string('profile_pic_url', 500).notNullable()
+    t.dateTime('created_at').notNullable().defaultTo(knex.fn.now())
+    t.dateTime('updated_at')
+    t.dateTime('last_active')
+    t.boolean('agreed_to_disclaimer').notNullable().defaultTo(false)
+    t.string('address')
+    t.float('latitude')
+    t.float('longitude')
+    t.text('description')
+  }).createTable('user_organizations', (t) => {
+    t.increments().primary()
+    t.string('userId').references('id').inTable('user')
+    t.string('organizationId').references('id').inTable('organization')
+  })
+}
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('user');
-};
+  return Promise.all([
+    knex.schema.dropTable('user_organizations'),
+    knex.schema.dropTable('organization'),
+    knex.schema.dropTable('user')
+  ])
+}
--- src/migrations/20181121205834_basicPermissions.js
+++ src/migrations/20181121205834_basicPermissions.js
@@ -1,27 +1,27 @@
-import knex from 'knex';
+import knex from 'knex'
 
 const basicPermissions = [
   { role: 'user', resource: 'ownUser', action: 'create' },
   { role: 'user', resource: 'ownUser', action: 'update' },
   { role: 'user', resource: 'ownUser', action: 'delete' },
-  { role: 'user', resource: 'ownUser', action: 'read' },
+  { role: 'user', resource: 'ownUser', action: 'read' }
 ]
 
 exports.up = async (knex, Promise) => {
   await knex.schema.createTable('permission', (t) => {
-    t.increments('id');
-    t.string('role', 100).notNullable();
-    t.string('resource', 100).notNullable();
-    t.string('action', 100).notNullable();
-    t.dateTime('created_at').notNullable();
-    t.dateTime('updated_at');
-  });
+    t.increments('id')
+    t.string('role', 100).notNullable()
+    t.string('resource', 100).notNullable()
+    t.string('action', 100).notNullable()
+    t.dateTime('created_at').notNullable()
+    t.dateTime('updated_at')
+  })
   return knex('permission').insert(basicPermissions.map((p) => {
-    p.created_at = new Date().toISOString(); 
-    return p;
-  }));
+    p.created_at = new Date().toISOString() 
+    return p
+  }))
 };
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('permission');
+  return knex.schema.dropTable('permission')
 };
