97493347385ac196ffc55bd0002c29064a888f34
anon <sarah.anne.friend@gmail.com>
2019-02-19 17:57:20 -0500

    Lil signer script working, needs cleanup

--- scripts/signTx.js
+++ scripts/signTx.js
@@ -1,23 +1,26 @@
 //var request = require('request');
-
+const Web3 = require('web3')
 const BlueBird = require('bluebird')
 const { generators, signers } = require('eth-signer')
 const TxRelaySigner = signers.TxRelaySigner
 const Transaction = require('ethereumjs-tx')
 
 const rpcUrl = process.env.RPC_URL || 'http://127.0.0.1:8545'
+const secretSeed = process.env.SEED
 
 const web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl))
+const HDSigner = signers.HDSigner
 
-const HubContractJSON = require('../../contracts/build/contracts/Hub.json')
-const TxRelayContractJSON = require('../../contracts/build/contracts/TxRelay.json')
+const HubJSON = require('../contracts/build/contracts/Hub.json')
+const HubFactoryJSON = require('../contracts/build/contracts/HubFactory.json')
+const TxRelayContractJSON = require('../contracts/build/contracts/TxRelay.json')
 
 const txRelayAddress = TxRelayContractJSON.networks[process.env.NETWORK_ID].address || process.env.RELAY_CONTRACT_ADDRESS
-const hubAddress = process.env.HUB_CONTRACT_ADDRESS
+const hubFactoryAddress = HubFactoryJSON.networks[process.env.NETWORK_ID].address || process.env.HUB_FACTORY_ADDRESS
 
-const HubContract = new web3.eth.Contract(
-  HubContractJSON.abi,
-  hubAddress
+const HubFactoryContract = new web3.eth.Contract(
+  HubFactoryJSON.abi,
+  hubFactoryAddress
 )
 
 const TxRelayContract = BlueBird.promisifyAll(new web3.eth.Contract(
@@ -25,21 +28,27 @@ const TxRelayContract = BlueBird.promisifyAll(new web3.eth.Contract(
   txRelayAddress
 ))
 
-const senderKeyPair = { address: web3.eth.accounts[0] }
+let senderKeyPair;
+let tx;
+let txRelaySigner;
+let HubContract;
+let hubAddress;
 
-const txRelaySigner = new TxRelaySigner(
-  senderKeyPair,
-  txRelayAddress,
-  senderKeyPair.address,
-  '0x0'
-)
+const getSender = async () => {
+  senderKeyPair = generators.KeyPair.fromPrivateKey(process.env.PRIV_KEY)
+  return senderKeyPair
+}
 
-const tx = {
-  from: senderKeyPair.address, 
-  to: hubAddress, 
-  value: 0,
-  data: HubContract.methods.signup(arg, arg2).encodeABI() 
-};
+const instantiateHub = async () => {
+  const events = await HubFactoryContract.getPastEvents("Spawn", { fromBlock: 0, toBlock: "latest" })
+  HubContract = new web3.eth.Contract(
+    HubJSON.abi,
+    events[0].returnValues.newHub
+  )
+  hubAddress = events[0].returnValues.newHub
+  return 
+}
+
 
 const sign = async (txParams) => {
   const txRelayNonce = await TxRelayContract.methods.getNonce(senderKeyPair.address).call()
@@ -50,20 +59,38 @@ const sign = async (txParams) => {
   // if (this.isWeb3Provided === true) {
   //   metaSignedTx = await this.txRelaySigner.signRawTx(rawTx, this.signFunction)
   // } else {
-  const metaSignedTx = await txRelaySigner.signRawTx(rawTx)
-  // }
-  const params = {
-    metaNonce: txParams.nonce,
-    metaSignedTx,
-    blockchain: 'blockchain'
-  }
-  console.log(params)
-  return metaSignedTx
+  console.log(rawTx)
+  txRelaySigner.signRawTx(rawTx, (err, metaSignedTx) => {
+    console.log(metaSignedTx)
+    const params = {
+      metaNonce: txParams.nonce,
+      metaSignedTx,
+    }
+    console.log(params)
+    return metaSignedTx
+  })
 }
 
-const signed = sign(tx, privateKey);
+getSender().then(async () => {
+  //console.log(hubFactoryAddress)
+  await instantiateHub()
+  txRelaySigner = new TxRelaySigner(
+    senderKeyPair,
+    txRelayAddress,
+    senderKeyPair.address,
+    '0x0'
+  )
+  tx = {
+    from: senderKeyPair.address, 
+    to: hubAddress,//hubAddress, 
+    value: 0,
+    data: HubContract.methods.signup('test').encodeABI() 
+  };
+  console.log(hubAddress)
+  const signed = await sign(tx);
+  console.log(signed)
+})
 
-console.log(signed)
 
 // const signFunction = async (hash) => {
 //   const sig = await web3.eth.signAsync(hash, address)
