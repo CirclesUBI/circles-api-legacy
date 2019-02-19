7a088b532f759eb330ad62f27a361b79edacf0c0
anon <sarah.anne.friend@gmail.com>
2019-02-19 11:01:09 -0500

    Ready to test with signing script

--- scripts/signTx.js
+++ scripts/signTx.js
@@ -25,39 +25,32 @@ const TxRelayContract = BlueBird.promisifyAll(new web3.eth.Contract(
   txRelayAddress
 ))
 
-// const tx = {
-//   // this could be provider.addresses[0] if it exists
-//   from: fromAddress, 
-//   // target address, this could be a smart contract address
-//   to: toAddress, 
-//   // optional if you want to specify the gas limit 
-//   gas: gasLimit, 
-//   // optional if you are invoking say a payable function 
-//   value: 0,
-//   // this encodes the ABI of the method and the arguements
-//   data: myContract.methods.myMethod(arg, arg2).encodeABI() 
-// };
-
-
 const senderKeyPair = { address: web3.eth.accounts[0] }
 
-txRelaySigner = new TxRelaySigner(
+const txRelaySigner = new TxRelaySigner(
   senderKeyPair,
   txRelayAddress,
   senderKeyPair.address,
   '0x0'
 )
 
+const tx = {
+  from: senderKeyPair.address, 
+  to: hubAddress, 
+  value: 0,
+  data: HubContract.methods.signup(arg, arg2).encodeABI() 
+};
+
 const sign = async (txParams) => {
   const txRelayNonce = await TxRelayContract.methods.getNonce(senderKeyPair.address).call()
   txParams.nonce = Web3.utils.toHex(txRelayNonce)
   const tx = new Transaction(txParams)
   const rawTx = '0x' + tx.serialize().toString('hex')
-  let metaSignedTx
+  //let metaSignedTx
   // if (this.isWeb3Provided === true) {
   //   metaSignedTx = await this.txRelaySigner.signRawTx(rawTx, this.signFunction)
   // } else {
-    metaSignedTx = await this.txRelaySigner.signRawTx(rawTx)
+  const metaSignedTx = await txRelaySigner.signRawTx(rawTx)
   // }
   const params = {
     metaNonce: txParams.nonce,
@@ -65,9 +58,12 @@ const sign = async (txParams) => {
     blockchain: 'blockchain'
   }
   console.log(params)
+  return metaSignedTx
 }
 
-const signPromise = web3.eth.accounts.signTransaction(tx, privateKey);
+const signed = sign(tx, privateKey);
+
+console.log(signed)
 
 // const signFunction = async (hash) => {
 //   const sig = await web3.eth.signAsync(hash, address)
--- src/connections/blockchain.js
+++ src/connections/blockchain.js
@@ -7,12 +7,11 @@ const rpcUrl = require('../config/env').rpcUrl
 const web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl))
 
 const HubAddress = process.env.HUB_CONTRACT_ADDRESS
-const TxRelayAddress = TxRelayContractJSON.networks[process.env.NETWORK_ID].address || process.env.RELAY_CONTRACT_ADDRESS
+const TxRelayAddress =
+  TxRelayContractJSON.networks[process.env.NETWORK_ID].address ||
+  process.env.RELAY_CONTRACT_ADDRESS
 
-const HubContract = new web3.eth.Contract(
-  HubContractJSON.abi,
-  HubAddress
-)
+const HubContract = new web3.eth.Contract(HubContractJSON.abi, HubAddress)
 
 const TxRelayContract = new web3.eth.Contract(
   TxRelayContractJSON.abi,
