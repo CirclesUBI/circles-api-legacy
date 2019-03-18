233bcdc4f51210c188f79b7076a8c39c2fb26872
shkfnly <ashoka.finley@gmail.com>
2019-03-19 00:36:43 +0900

    export functions to module

--- src/controllers/relayerController.js
+++ src/controllers/relayerController.js
@@ -1,9 +1,13 @@
 const logger = require('../lib/logger')
-const relayer = require('../lib/relayer')
+const MetaTxHandler = require('metatx-server')
+const apiPrivKey = process.env.API_PRIV_KEY
+const { provider, txRelayAddress, txRelayABI } = require('../connections/blockchain')
+
+const metaTxHandler = new MetaTxHandler(apiPrivKey, provider, txRelayAddress, txRelayABI, logger)
 
 const relay = async (req, res) => {
   try {
-    const result = await relayer.handle(req)
+    const result = await MetaTxHandler.handle(req)
     logger.info('This the transactionHash', result)
     res.status(200).json({ status: 'success', data: result })
   } catch (error) {
