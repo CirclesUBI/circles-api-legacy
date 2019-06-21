144ab3b22aa36e8705080c5aac6258abce10033e
edzillion <edzillion@gmail.com>
2019-06-21 10:00:04 +0200

    log network id

--- src/connections/blockchain.js
+++ src/connections/blockchain.js
@@ -6,6 +6,8 @@ const rpcUrl = require('../config/env').rpcUrl
 const provider = new Web3.providers.HttpProvider(rpcUrl)
 const web3 = new Web3(provider)
 
+console.log('process.env.NETWORK_ID', process.env.NETWORK_ID)
+
 const txRelayAddress =
   TxRelayContractJSON.networks[process.env.NETWORK_ID].address ||
   process.env.RELAY_CONTRACT_ADDRESS
